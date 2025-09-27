ce667f7686fb483fa1a7d2fdc2fa726bfc5246b7
ubicoders <ubicoders.0@gmail.com>
2025-09-27 16:15:17 -0700

    updated

--- install_py.bash
+++ install_py.bash
@@ -4,7 +4,6 @@ set -e  # exit on error
 # First build
 python -m build --wheel
 
-
 # Run stub generation if "stubs/" is missing OR "gen-stub" is passed
 if [[ ! -d "stubs" || "$1" == "gen-stub" ]]; then
     pip install dist/*.whl
@@ -14,12 +13,12 @@ if [[ ! -d "stubs" || "$1" == "gen-stub" ]]; then
 
     # Rebuild after stubs are copied
     python -m build --wheel
-    pip install --force-reinstall dist/pyg2o-*.whl
+    pip install --force-reinstall dist/ubicoders_g2opy-*.whl
     pip install dist/*.whl
 fi
 
 # Fix wheel for PyPI
 pip install auditwheel
-auditwheel repair dist/pyg2o-*-linux_x86_64.whl -w dist/
-rm dist/pyg2o-*-linux_x86_64.whl
-pip install --force-reinstall dist/pyg2o*.whl
+auditwheel repair dist/ubicoders_g2opy-*-linux_x86_64.whl -w dist/
+rm dist/ubicoders_g2opy-*-linux_x86_64.whl
+pip install --force-reinstall dist/ubicoders_g2opy-*manylinux_2_39_x86_64.whl
