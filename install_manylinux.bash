22f06cdecb46296724559aa2dd5990645b17a6a6
ubicoders <ubicoders.0@gmail.com>
2025-09-27 14:43:22 -0700

    updated

---
+++ install_manylinux.bash
@@ -0,0 +1,3 @@
+pip install auditwheel
+auditwheel repair g2opy-2.1.0-cp312-cp312-linux_x86_64.whl -w dist/
+rm dist/g2opy-2.1.0-cp312-cp312-linux_x86_64.whl
--- install_py.bash
+++ install_py.bash
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
-python -m build --wheel
-pip install --force-reinstall dist/g2opy-*.whl
-pip install dist/*.whl
+set -e  # exit on error
 
-pybind11-stubgen g2opy -o stubs
-rsync -a stubs/g2opy/ python/g2opy/
-
+# First build
 python -m build --wheel
-pip install --force-reinstall dist/g2opy-*.whl
-pip install dist/*.whl
+
+
+# Run stub generation if "stubs/" is missing OR "gen-stub" is passed
+if [[ ! -d "stubs" || "$1" == "gen-stub" ]]; then
+    pip install dist/*.whl
+    echo "Generating stubs..."
+    pybind11-stubgen g2opy -o stubs
+    rsync -a stubs/g2opy/ python/g2opy/
+
+    # Rebuild after stubs are copied
+    python -m build --wheel
+    pip install --force-reinstall dist/g2opy-*.whl
+    pip install dist/*.whl
+fi
+
+# Fix wheel for PyPI
+pip install auditwheel
+auditwheel repair dist/g2opy-*-linux_x86_64.whl -w dist/
+rm dist/g2opy-*-linux_x86_64.whl
+pip install --force-reinstall dist/g2opy*.whl
